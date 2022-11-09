8daa7f795a17606d37e0cef024a9e3994f7f05a7
kimjbstar <kimjbstar@gmail.com>
2022-11-09 21:30:24 +0900

    wip

--- src/convertor.ts
+++ src/convertor.ts
@@ -333,6 +333,12 @@ export class PrismaConvertor {
 				} else if (dmmfField.type === 'String') {
 					field.default = `'${field.default}'`
 				}
+			} else if (Array.isArray(dmmfField.default)) {
+				if (dmmfField.type === 'String') {
+					field.default = `[${dmmfField.default.toString()}]`
+				} else {
+					field.default = `[${dmmfField.default.toString()}]`
+				}
 			}
 		}
 
