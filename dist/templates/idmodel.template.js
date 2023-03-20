8a7594792a39719dc34bf4bc1ced3832d2d7b6c0
Maréchal Eliot <eliot.marechal@gmail.com>
2023-03-20 12:10:29 +0100

    FIX 'this' in static method....

--- dist/templates/idmodel.template.js
+++ dist/templates/idmodel.template.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.IDMODEL_TEMPLATE = void 0;
 exports.IDMODEL_TEMPLATE = `static async fromId(id: number): Promise<#!{NAME} | null> {
-  const dbModel = await this.model.findUnique({
+  const dbModel = await #!{NAME}.model.findUnique({
     where:{
       #!{FIELD_NAME}: id
     }
--- src/templates/idmodel.template.ts
+++ src/templates/idmodel.template.ts
@@ -1,5 +1,5 @@
 export const IDMODEL_TEMPLATE = `static async fromId(id: number): Promise<#!{NAME} | null> {
-  const dbModel = await this.model.findUnique({
+  const dbModel = await #!{NAME}.model.findUnique({
     where:{
       #!{FIELD_NAME}: id
     }
