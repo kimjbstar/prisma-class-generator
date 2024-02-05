aef816959d178cb6e7794ff3c560ccb8fc088885
Jannis6023 <info@jannisgollwitzer.de>
2024-02-05 18:36:11 +0100

    fixed bug for 1:1 self-relations in separate relation classes

--- src/convertor.ts
+++ src/convertor.ts
@@ -212,7 +212,7 @@ export class PrismaConvertor {
 		const relationTypes = uniquify(
 			model.fields
 				.filter(
-					(field) => field.relationName && model.name !== field.type,
+					(field) => field.relationName && (this._config.separateRelationFields ? true : model.name !== field.type),
 				)
 				.map((v) => v.type),
 		)
